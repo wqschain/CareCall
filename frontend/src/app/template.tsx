53f9c36be1112e9d11335d332a7cfc2d277318c1
Waqas Rana <waqasrana@Waqass-Laptop.local>
2025-06-27 23:20:25 -0400

    Update template.tsx to use new ThemeProvider props

---
+++ frontend/src/app/template.tsx
@@ -0,0 +1,14 @@
+import { UserProvider } from '@auth0/nextjs-auth0/client'
+import { ThemeProvider } from '@/components/theme-provider'
+import { Toaster } from '@/components/ui/toaster'
+
+export default function Template({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider defaultTheme="system" storageKey="carecall-theme">
+      <UserProvider>
+        {children}
+        <Toaster />
+      </UserProvider>
+    </ThemeProvider>
+  )
+} 
