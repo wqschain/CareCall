a03f4492b9e04771b7a711842181be26d72478cc
Waqas Rana <waqasrana@Waqass-Laptop.local>
2025-06-28 09:39:59 -0400

    fix: prevent static generation of auth routes

--- frontend/src/app/api/auth/logout/route.ts
+++ frontend/src/app/api/auth/logout/route.ts
@@ -1,5 +1,9 @@
 import { handleAuth, handleLogout } from '@auth0/nextjs-auth0';
 
+export const dynamic = 'force-dynamic';
+
 export const GET = handleAuth({
-  logout: handleLogout({ returnTo: process.env.AUTH0_BASE_URL })
+  logout: handleLogout({ 
+    returnTo: process.env.AUTH0_BASE_URL || 'https://carecall.club'
+  })
 }); 
