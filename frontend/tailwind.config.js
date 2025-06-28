29cf350c578082b459e9d39f55d5aa084f7fb8d8
Waqas Rana <waqasrana@Waqass-Laptop.local>
2025-06-28 04:21:26 -0400

    fix: restore frontend configuration files

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,9 +2,9 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './src/pages/**/*.{ts,tsx}',
-    './src/components/**/*.{ts,tsx}',
-    './src/app/**/*.{ts,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     container: {
