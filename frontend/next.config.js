a969a1bc2a0e3064a8127f4980f9caa138713812
Waqas Rana <waqasrana@Waqass-Laptop.local>
2025-06-28 22:39:06 -0400

    fix: update import paths and next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -19,6 +19,13 @@ const nextConfig = {
       }
     ]
   },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, './src'),
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig 
